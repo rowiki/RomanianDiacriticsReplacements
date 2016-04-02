ead865ed98889677bacfb8e46a8ad6cf0215d0b1
Strainu <github@strainu.ro>
2016-04-02 15:10:37 +0300

    Initial version of the extension.
    It doesn't do any text transformation yet.

---
+++ RomanianDiacriticsReplacements_body.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copyright 2016 Strainu <http://www.strainu.ro/>
+ *
+ *
+ * This source code is under the terms of the
+ * GNU General Public License. 
+ * see <http://www.gnu.org/licenses/gpl.txt>
+ */
+
+class RomanianDiacriticsReplacements {
+	public static function attemptSave($m_pageObj) {
+		return true;
+	}
+}
